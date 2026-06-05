36bc11d9aa0055fe5d88c29557afebfe3ac41019
Shus13 <sushitkarki234@gmail.com>
2026-06-05 22:01:21 +0545

    add home UI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 
-import express from 'express'
+import express, { type Request, type Response } from 'express'
+import path from 'path'
 const app = express()
+
+app.set("views", path.join(process.cwd(), "views"));app.set("view engine", "ejs")
+app.get("/", (req:Request, res:Response)=> {
+    res.render("home.ejs")
+})
 
 export default app
