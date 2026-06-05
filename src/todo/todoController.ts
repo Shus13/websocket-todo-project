cedbebdd5c64778e1f0f6e11c397ca2f07c80d72
Shus13 <sushitkarki234@gmail.com>
2026-06-05 19:00:24 +0545

    add handleUpdateStatus

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,7 +1,11 @@
 import type { Socket } from "socket.io";
 import { getSocketIo } from "../../server.js";
 import todoModel from "./todoModel.js";
-import type { IToDo } from "./todoTypes.js";
+import { Status, type IToDo } from "./todoTypes.js";
+
+function parseData(data: any) {
+  return typeof data === "string" ? JSON.parse(data) : data;
+}
 
 class Todo {
   init() {
@@ -9,45 +13,28 @@ class Todo {
 
     io.on("connection", (socket: Socket) => {
       console.log("New client connected");
-      socket.on("addTodo", (data) => this.handleAddTodo(socket, data));
-      socket.on("deleteTodo", (data) => this.handleDeleteTodo(socket, data));
+
+      socket.on("addTodo", (data) =>this.handleAddTodo(socket, data));
+      socket.on("deleteTodo", (data) =>this.handleDeleteTodo(socket, data));
+      socket.on("updateTodoStatus", (data) =>this.handleUpdateTodoStatus(socket, data));
     });
   }
 
   private async handleAddTodo(socket: Socket, data: IToDo) {
     try {
-      const { task, deadline, status } = data;
+      const parsedData = parseData(data);
+      const { task, deadline, status } = parsedData;
 
-      await todoModel.create({
+      const todo = await todoModel.create({
         task,
         deadline,
         status,
       });
-      const todos = await todoModel.find();
-      socket.emit("todos_updated", {
-        status: "Success",
-        data: todos,
-      });
-    } catch (error) {
-      socket.emit("todo_response", {
-        status: "Error",
-        message: "Failed to create todo",
-      });
-    }
-  }
 
-  private async handleDeleteTodo(socket: Socket, data: { id: String }) {
-    try {
-      const { id } = data;
-      const deleteTodo = await todoModel.findByIdAndDelete(id);
-      if (!deleteTodo) {
-        socket.emit("todo_response", {
-          status: "error",
-          message: "Todo not found",
-        });
-        return;
-      }
-      const todos = await todoModel.find();
+      const todos = await todoModel.find({
+        status: Status.Pending,
+      });
+
       socket.emit("todos_updated", {
         status: "success",
         data: todos,
@@ -56,6 +43,80 @@ class Todo {
       socket.emit("todo_response", {
         status: "error",
         message: "Failed to create todo",
+      });
+    }
+  }
+
+  private async handleDeleteTodo(
+    socket: Socket,
+    data: { id: string }
+  ) {
+    try {
+      const parsedData = parseData(data);
+      const { id } = parsedData;
+
+      const deletedTodo =
+        await todoModel.findByIdAndDelete(id);
+
+      if (!deletedTodo) {
+        socket.emit("todo_response", {
+          status: "error",
+          message: "Todo not found",
+        });
+        return;
+      }
+
+      const todos = await todoModel.find({
+        status: Status.Pending,
+      });
+
+      socket.emit("todos_updated", {
+        status: "success",
+        data: todos,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: "error",
+        message: "Failed to delete todo",
+      });
+    }
+  }
+
+  private async handleUpdateTodoStatus(
+    socket: Socket,
+    data: { id: string; status: Status }
+  ) {
+    try {
+      const parsedData = parseData(data);
+      const { id, status } = parsedData;
+
+      const todo =
+        await todoModel.findByIdAndUpdate(
+          id,
+          { status },
+          { new: true }
+        );
+
+      if (!todo) {
+        socket.emit("todo_response", {
+          status: "error",
+          message: "Todo not found",
+        });
+        return;
+      }
+
+      const todos = await todoModel.find({
+        status: Status.Pending,
+      });
+
+      socket.emit("todo_updated", {
+        status: "success",
+        data: todos,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: "error",
+        message: "Error updating todo",
       });
     }
   }
